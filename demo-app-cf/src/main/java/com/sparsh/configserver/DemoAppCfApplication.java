dd81f6fbefb2d41012641bc06047f9b13add916a
spa0807 <sparshupadhyay@gmail.com>
2022-04-20 19:39:50 +0530

    Initial Commit

---
+++ demo-app-cf/src/main/java/com/sparsh/configserver/DemoAppCfApplication.java
@@ -0,0 +1,13 @@
+package com.sparsh.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppCfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppCfApplication.class, args);
+	}
+
+}
---
+++ demo-app-cf/src/test/java/com/sparsh/configserver/DemoAppCfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparsh.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppCfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
